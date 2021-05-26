079ae57670aea3181b09c75b7e493e9a296ef640
Elnur-web <elnurruzmanov219autp@gmail.com>
2021-05-26 17:09:43 +0500

    add

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
-const videoElement = document.querySelector ("#video")
+window.navigator.geolocation.getCurrentPosition(async  success =>{
+    const lat = success.coords.latitude
+    const long = success.coords.longitude 
+    const tbodyElement = document.querySelector(".tbody")
+    const cityElement = document.querySelector (".city  ")
+
+    let response = await fetch (`https://api.aladhan.com/v1/timings/calendar?latitude=${lat}&longitude=${long}&method=3&school=1&month=${new Date().getMonth()}&year=${new Date().getFullYear()}`)
+    response =await response.json()
+
+     let times = ["Dhuhr", "Fajr", "Asr", "Isha", "Maghrib"]
 
 
-async function main(){
-    let media = await window.navigator.mediaDevices.getUserMedia({ video:true })
+     cityElement.textContent = `(${response.data.meta.timezone})`
 
-    videoElement.srcObject = media
-}
+    for(let data in response.data.timings){
+        if(times.indexOf(data) !==-1){
+        const newTrElement = document.createElement('tr')
+        const newNameElement = document.createElement('td')
+        const newTimeElement = document.createElement('td')
+        newNameElement.textContent = data
+        newTimeElement.textContent = response.data.timings[data]
+        newTrElement.appendChild(newNameElement)
+        newTrElement.appendChild(newTimeElement)
+        tbodyElement.appendChild(newTrElement)
 
-main()
+
+        console.log(data, response.data.timings[data]);
+        }
+    }
+
+
+}, error =>{
+    alert ("You don't give permission to get location")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
